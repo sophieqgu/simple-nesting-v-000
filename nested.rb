b9ea8be13dd7544c103d549233995857a8f91581
Sophie Gu <sophieqgu@gmail.com>
2018-10-12 23:41:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -102,9 +102,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash[:alan_kay][:known_for]
      
 end
 
@@ -124,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+   programmer_hash[:dennis_ritchie][:languages]
 end
