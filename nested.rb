e1deffd6fb617e7b5aaebee94b1217e9e3d33261
Sophie Gu <sophieqgu@gmail.com>
2018-10-12 23:54:04 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,7 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }
-  programmer_hash[:yukihiro_matsumoto].keys
+  programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
