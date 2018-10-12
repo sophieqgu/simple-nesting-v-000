e80ab7ebfe7e33a8a4bd7d46844de37791983e64
Sophie Gu <sophieqgu@gmail.com>
2018-10-12 23:42:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
-  }]
+  }
   programmer_hash[:yukihiro_matsumoto]  
 end
 
@@ -124,5 +124,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-   programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages]
 end
